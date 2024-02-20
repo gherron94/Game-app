d300dd34eda4efc98908215797fdbf4366c257b7
Sanya Hussain <sanz_133@hotmail.com>
2024-02-20 13:55:42 +0000

    leaderboard done for now, need to fix sound issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import MainScreen from './components/MainScreen'
-import Settings from './components/Settings'
-import LeaderBoards from './components/LeaderBoards';
-import Controls from './components/Controls';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import MainScreen from "./components/MainScreen";
+import Settings from "./components/Settings";
+import LeaderBoards from "./components/LeaderBoards";
+import Controls from "./components/Controls";
 
 const App: React.FC = () => {
- 
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainScreen title='Untitled Game'/>} />
-        <Route path="/settings" element={<Settings title = 'Game Settings'/>} />
-        <Route path="/leaderboards" element={<LeaderBoards title = 'Leaderboards'/>} />
-        <Route path="/controls" element={<Controls title = 'Controls'/>} />
+        <Route path="/" element={<MainScreen title="Untitled Game" />} />
+        <Route path="/settings" element={<Settings title="Game Settings" />} />
+        <Route
+          path="/leaderboards"
+          element={<LeaderBoards title="Leaderboard" />}
+        />
+        <Route path="/controls" element={<Controls title="Controls" />} />
       </Routes>
     </>
-  )
-} 
+  );
+};
 
-export default App
+export default App;
--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,36 +1,38 @@
 import { MainScreenProps } from "../App.types";
 import { NavLink } from "react-router-dom";
 
-const MainScreen: React.FC<MainScreenProps> = ({title}) => {
-
+const MainScreen: React.FC<MainScreenProps> = ({ title }) => {
   return (
     <>
-      <div className='container'>
-      <div className='main'>
-      <header>
-        <h1>{title}</h1>
-      </header>
-      <main>
-      <img src="" alt='Undecided main screen image'/>
-      <div className='start-button'>
-        <h2>Start Game</h2>
+      <div className="container">
+        <div className="main">
+          <header>
+            <h1>{title}</h1>
+          </header>
+          <main>
+            <img src="" alt="Undecided main screen image" />
+            <div className="start-button">
+              <h2>Start Game</h2>
+            </div>
+            <NavLink to="/leaderboards">
+              <button>Leaderboard</button>
+            </NavLink>
+            <NavLink to="/controls">
+              <button>Controls</button>
+            </NavLink>
+            <NavLink to="/settings">
+              <button>Settings</button>
+            </NavLink>
+          </main>
+        </div>
+        <div className="footer">
+          <footer>
+            <p>© 2024 Group E Untitled Game</p>
+          </footer>
+        </div>
       </div>
-      <NavLink to='/leaderboards'><button>Leaderboards</button></NavLink>
-      <NavLink to='/controls'><button>Controls</button></NavLink>
-      <NavLink to='/settings'><button>Settings</button></NavLink>
-      </main>
-      </div>
-      <div className='footer'>
-      <footer>
-        <p>
-        © 2024 Group E Untitled Game
-        </p>
-      </footer>
-      </div>
-    </div>
     </>
-  )
-
-} 
+  );
+};
 
 export default MainScreen;
